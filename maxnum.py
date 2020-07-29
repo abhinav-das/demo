f9f6e6b10db4f1a11f9dfaba6c1d0844d1dc4d2c
root <jagadish.patra@calsoftinc.com>
2020-07-29 15:21:35 +0530

    version 1 of maxnum

---
+++ maxnum.py
@@ -0,0 +1,11 @@
+a = []
+n = int(input("enter the size of list : "))
+for i in range(0,n):
+	ele = int(input("enter element: "))
+	a.append(ele)
+
+print(a)
+print 'max number in the list is ',max(a)
+
+
+
